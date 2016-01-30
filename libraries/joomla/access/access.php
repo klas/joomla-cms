9f3867f92f912f2fda004847e90a69c0f2fffe92
klas <klas.berlic@gmail.com>
2016-01-30 17:19:46 +0100

    some doc blocks

--- libraries/joomla/access/access.php
+++ libraries/joomla/access/access.php
@@ -362,6 +362,8 @@ class JAccess
 
 	/**
 	 * Merge new permissions with old rules from assets table for backwards compatibility
+	 *
+	 * @param $results db query result object with persmissions and rules
 	 */
 	private function mergePermissionsRules($results)
 	{
